70dd6f6b69430200c35b3798d6b483f049a3df95
FetraFex <nyfetra.phanoel@gmail.com>
2024-09-15 20:28:15 +0300

    React-Three-Tailwindcss Ready Template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="h-screen flex justify-center items-center ">
+      <div className="border w-1/2 p-6">
+        <h1 className="text-9xl text-center text-green-800 font-bold uppercase">Hello World</h1>
+        <h1 className="text-center font-medium mt-5">This is a template for REACT - Three - Tailwind Project</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
