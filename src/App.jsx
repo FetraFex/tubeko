2906c0b0577a9a69e640e530b5131a320cee9b71
FetraFex <nyfetra.phanoel@gmail.com>
2025-01-02 20:48:05 +0300

    Initialization: Tubeko project initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Main from "./components/Main"
-import Landing from "./components/Landing"
-import Parking from "./components/Parking"
-import Yuka from "./components/yuka"
-import Test from "./components/Test"
 
 function App() {
 
   return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<Landing />}/>
-          <Route path="/parking" element={<Parking />}/>
-          <Route path="/park" element={<Test />}/>
-        </Routes>
-    </Router>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
