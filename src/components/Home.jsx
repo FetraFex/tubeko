49c51681f94c982e0cd1d2624aafae453b89d163
FetraFex <nyfetra.phanoel@gmail.com>
2025-02-23 21:20:06 +0300

    Implement YouTube API integration to fetch playlist items

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,79 @@
-import { faArrowDown, faArrowRight, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown, faArrowRight, faChevronDown, faDownload } from '@fortawesome/free-solid-svg-icons'
 import { faClose } from '@fortawesome/free-solid-svg-icons/faClose'
 import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import { motion } from "framer-motion";
+
 const Home = () => {
-    const [users, setUsers] = useState([])
-    const [error, setError] = useState(null)
+    const [playlistId, setPlaylistId] = useState("");
+    const [videos, setVideos] = useState([])
+    const morphVariants = {
+        animate: {
+            d: [
+                "M 0 300 Q 150 100 300 300 T 600 300 T 900 300 T 1200 300 V 800 H 0 Z",
+                "M 0 400 Q 200 200 400 400 T 800 400 T 1200 400 V 800 H 0 Z",
+                "M 0 300 Q 150 100 300 300 T 600 300 T 900 300 T 1200 300 V 800 H 0 Z"
+            ],
+            transition: {
+                duration: 10,
+                repeat: Infinity,
+                ease: "easeInOut"
+            }
+        }
+    };
 
-    useEffect(() => {
-        axios
-        .get('http://localhost:3000/users')
-        .then(response => setUsers(response.data))
-        .catch(err => {
-            console.log(err.message);
-            
-        })
-    }, [])
-
-    const showUsers = () => {
-        console.log(users);
-        
+    const handleFetchVideos = async () => {
+        try {
+            const response = await axios.get(`http://localhost:3000/api/playlist/${playlistId}`)
+            console.log(response.data.videos)
+        } catch (error) {
+            console.log("Error fetching videos: ", error.message);
+        }
     }
+
     return (
-        <div className='h-screen bg-red-300 flex flex-col justify-center items-center text-center relative z-0'>
-            {users.map(user => (
-                <li key={user.id}>{user.username}</li>
-            ))}
-            <p>Streamline Your Downloads</p>
-            <h1 className='text-6xl font-bold'>Effortlessly Download and Enjoy<br />Your YouTube Playlists</h1>
-            <div className='flex bg-blue-600 w-1/2 rounded-full'>
-                <div className='py-5 w-1/12 rounded-s-full flex justify-center'><FontAwesomeIcon icon={faSearch} className='text-2xl' /></div>
-                <div className='w-10/12'><input type="text" className='h-full w-full px-5 text-xl' /></div>
-                <div className='py-5 w-1/12 rounded-e-full flex justify-center'><FontAwesomeIcon icon={faClose} className='text-2xl' /></div>
-            </div>
-            <div>
-                <button className='bg-blue-700'>Start <FontAwesomeIcon icon={faArrowRight} /></button>
-                <button className='bg-blue-700'>Quality <FontAwesomeIcon icon={faChevronDown} /></button>
+        <div className='m h-screen bg-red-300 flex flex-col justify-center items-center text-center relative z-0'>
+            <svg className="absolute z-0 top-0 left-1/2 -translate-x-1/2" width="800" height="800" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="m 582.14 726.18 
+       c 145.53 -194.04 48.51 -291.06 -242.55 -533.61 
+       c -291.06 -339.57 -339.57 -145.53 -339.57 -48.51 
+       q 0 145.53 97.02 242.55 
+       c 97.02 145.53 194.04 485.1 485.1 339.57 
+       z"
+                    stroke="none"
+                    fill="lightblue"
+                    strokeWidth="2"
+                />
+            </svg>
+            <svg
+                className="absolute z-0 top-1/2 left-0 -translate-x-1/3 -translate-y-1/3"
+                width="800"
+                height="800"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="m 325 20 c -175 -50 -275 0 -325 175 c 0 125 75 375 300 300 c 100 -50 200 -150 150 -250 q 0 -75 -75 -175 c -16.6667 -16.6667 -33.3333 -33.3333 -50 -50 z"
+                    stroke="none"
+                    fill="lightblue"
+                    strokeWidth="2"
+                />
+            </svg>
+
+            <p className='z-50'>Streamline Your <span className='px-2 py-1 bg-slate-400 rounded-full'><FontAwesomeIcon icon={faDownload} /> Downloads</span></p>
+            <div className='z-50 w-full justify-center items-center flex flex-col space-y-6'>
+                <h1 className='text-6xl font-bold'>Effortlessly Download and Enjoy<br />Your YouTube Playlists</h1>
+                <div className='flex bg-blue-600 w-1/2 rounded-full'>
+                    <div className='py-4 w-1/12 rounded-s-full flex justify-center'><FontAwesomeIcon icon={faSearch} className='text-2xl' /></div>
+                    <div className='w-10/12'><input onChange={(e) => setPlaylistId(e.target.value)} type="text" className='h-full w-full px-5 text-xl' placeholder='Paste the video or the playlist URL here ! 😉' /></div>
+                    <div className='py-4 w-1/12 rounded-e-full flex justify-center'><FontAwesomeIcon icon={faClose} className='text-2xl' /></div>
+                </div>
+                <div className='flex space-x-2'>
+                    <button onClick={handleFetchVideos} className='bg-blue-700 pl-4 pr-2 py-2 rounded-xl font-medium flex justify-between items-center space-x-2'><span>Start conversion</span><FontAwesomeIcon className="bg-green-400 p-3 rounded-xl" icon={faArrowRight} /></button>
+                    <button className='bg-blue-700 px-4 rounded-xl font-medium border-2'>Quality <FontAwesomeIcon icon={faChevronDown} /></button>
+                </div>
             </div>
             <div className='absolute bottom-8 w-full flex justify-between px-36'>
                 <div className="flex">
@@ -48,8 +85,8 @@ const Home = () => {
                         <p>A</p>
                     </div>
                 </div>
-                <div className='flex items-center' onClick={showUsers}>
-                    <p>Scroll to explore</p><FontAwesomeIcon icon={faArrowDown} />
+                <div className='flex items-center space-x-1'>
+                    <p><span className="font-medium">Scroll</span> to explore</p><FontAwesomeIcon icon={faArrowDown} />
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
+import { faChevronDown, faGlobe, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 w-full bg-slate-200 px-36 py-10 flex justify-between z-50'>
+    <div className='fixed top-0 w-full bg-slate-200 px-36 py-6 flex justify-between items-center z-50'>
       <div className='flex space-x-24'>
         <h1>Tubeko</h1>
         <ul className='flex gap-8'>
-          <li>Home</li>
-          <li>How to Use/FAQ</li>
-          <li>Features</li>
-          <li>Supported Formats</li>
+          <li className='font-semibold'>Home</li>
+          <li className='font-semibold'>How to Use/FAQ</li>
+          <li className='font-semibold'>Features</li>
+          <li className='font-semibold'>Supported Formats</li>
         </ul>
       </div>
-      <div className='flex gap-8'>
-        <div>English</div>
-        <div>
-          <h4>Start a challenge</h4>
+      <div className='flex gap-5 items-center'>
+        <div className='flex space-x-1 bg-white rounded-xl px-2 py-2'><span><FontAwesomeIcon icon={faGlobe} /></span><p className='font-medium'>English</p><span><FontAwesomeIcon icon={faChevronDown} /></span></div>
+        <div className='flex items-center space-x-1 bg-white rounded-xl px-2 py-2'>
+          <h4 className='font-medium'>Donate</h4>
+          <FontAwesomeIcon className="text-pink-500" icon={faHeart} />
         </div>
       </div>
     </div>
